ad1ab8b35f318cb6c781aeefb8f8afc8c9baf970
Agne <agne@lukoseviciute.com>
2019-09-09 19:35:44 +0300

    refactoring and starting testing

--- src/main/java/com/lukoseviciute/programming/util/CompareHelper.java
+++ src/main/java/com/lukoseviciute/programming/util/CompareHelper.java
@@ -3,87 +3,84 @@ package com.lukoseviciute.programming.util;
 import com.lukoseviciute.programming.models.Athlete;
 import com.lukoseviciute.programming.models.Mismatch;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class CompareHelper {
 
-    private static ArrayList<Mismatch> diffsInfo = new ArrayList<Mismatch>();
 
-    public static void checkForDifferences(List<Athlete> csvAthletes, List<Athlete> otherAthletes, String otherFileType){
+
+    //return diffsInfo
+
+    /**
+     *
+     * @param csvAthletes
+     * @param otherAthletes
+     * @param otherFileType
+     */
+    public static List<Mismatch> checkForDifferences(List<Athlete> csvAthletes, List<Athlete> otherAthletes, String otherFileType){
 
         int otherIndex = 0;
+
+        List<Mismatch> diffsInfo = new ArrayList<>();
 
         for (int i = 0; i < csvAthletes.size() ; i++) {
 
             if (!csvAthletes.get(i).getName().equals(otherAthletes.get(otherIndex).getName())){
                 PrintDifferences.printEntryMissing(csvAthletes.get(i).getName(), otherFileType);
+                diffsInfo.add(new Mismatch(csvAthletes.get(i).getName(), "not found", null, null));
             }
             else {
-                checkAllAttributes(csvAthletes.get(i), otherAthletes.get(otherIndex));
+                diffsInfo.addAll(checkAllAttributes(csvAthletes.get(i), otherAthletes.get(otherIndex)));
                 otherIndex++;
             }
         }
 
-        PrintDifferences.printDiffs(diffsInfo, otherFileType);
+        //csvAthletes.forEach(i -> )
+        return diffsInfo;
     }
 
-    public static void checkAllAttributes(Athlete csvAthlete, Athlete otherAthlete) {
-        checkRank(csvAthlete, otherAthlete);
-        checkMark(csvAthlete, otherAthlete);
-        checkDate(csvAthlete, otherAthlete);
-        checkLocation(csvAthlete, otherAthlete);
-    }
+    public static List<Mismatch> checkAllAttributes(Athlete csvAthlete, Athlete otherAthlete) {
+        List<Mismatch> klaidos = new ArrayList<>();
 
-
-    public static void checkRank(Athlete csvAthlete, Athlete otherAthlete) {
-        if (csvAthlete.getRank() != otherAthlete.getRank()){
-            Mismatch tweak = new Mismatch
-                    (csvAthlete.getName(), "Rank", Integer.toString(csvAthlete.getRank()), Integer.toString(otherAthlete.getRank()));
-            diffsInfo.add(tweak);
+        if (!checkRank(csvAthlete, otherAthlete)){
+            klaidos.add(new Mismatch
+                    (csvAthlete.getName(), "Rank", Integer.toString(csvAthlete.getRank()), Integer.toString(otherAthlete.getRank())));
         }
-        else
-            return;
+        if (!checkMark(csvAthlete, otherAthlete)){
+            klaidos.add(new Mismatch
+                    (csvAthlete.getName(), "Mark", csvAthlete.getMark(), otherAthlete.getMark()));
+        }
+        if (!checkDate(csvAthlete, otherAthlete)){
+            klaidos.add(new Mismatch
+                    (csvAthlete.getName(), "Date", csvAthlete.getDate(), otherAthlete.getDate()));
+        }
+        if (!checkLocation(csvAthlete, otherAthlete)){
+            klaidos.add(new Mismatch
+                    (csvAthlete.getName(), "Location", csvAthlete.getLocation(), otherAthlete.getLocation()));
+        }
+
+        return klaidos;
+
     }
 
-    public static void checkMark(Athlete csvAthlete, Athlete otherAthlete) {
-        if (!(csvAthlete.getMark().equals(otherAthlete.getMark()))) {
-            Mismatch tweak = new Mismatch
-                    (csvAthlete.getName(), "Mark", csvAthlete.getMark(), otherAthlete.getMark());
-            diffsInfo.add(tweak);
-        }
-        else
-            return;
+
+    public static boolean checkRank(Athlete csvAthlete, Athlete otherAthlete) {
+        return (csvAthlete.getRank() == otherAthlete.getRank());
     }
 
-   //Names must match, otherwise unable to check for missing values
-    /* public static void checkName(Athlete csvAthlete, Athlete otherAthlete) {
-        if (!(csvAthlete.getName().equals(otherAthlete.getName()))) {
-            Mismatch tweak = new Mismatch
-                    (csvAthlete.getName(), "Name", csvAthlete.getName(), otherAthlete.getName());
-            diffsInfo.add(tweak);
-        }
-        else
-            return;
-    }*/
-
-    public static void checkDate(Athlete csvAthlete, Athlete otherAthlete) {
-        if (!(csvAthlete.getDate().equals(otherAthlete.getDate()))) {
-            Mismatch tweak = new Mismatch
-                    (csvAthlete.getName(), "Date", csvAthlete.getDate(), otherAthlete.getDate());
-            diffsInfo.add(tweak);
-        }
-        else
-            return;
+    public static boolean checkMark(Athlete csvAthlete, Athlete otherAthlete){
+        return (csvAthlete.getMark().equals(otherAthlete.getMark()));
     }
 
-    public static void checkLocation(Athlete csvAthlete, Athlete otherAthlete) {
-        if (!(csvAthlete.getLocation().equals(otherAthlete.getLocation()))) {
-            Mismatch tweak = new Mismatch
-                    (csvAthlete.getName(), "Location", csvAthlete.getLocation(), otherAthlete.getLocation());
-            diffsInfo.add(tweak);
-        }
-        else
-            return;
+
+    public static boolean checkDate(Athlete csvAthlete, Athlete otherAthlete) {
+        return ((csvAthlete.getDate().equals(otherAthlete.getDate())));
     }
+
+    public static boolean checkLocation(Athlete csvAthlete, Athlete otherAthlete) {
+        return ((csvAthlete.getLocation().equals(otherAthlete.getLocation())));
+    }
+
 }
